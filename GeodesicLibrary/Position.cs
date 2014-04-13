c3c74143f47bc5064039f13d66bcdd4aaa1435d5
Mark Johnson <marknotgeorge@googlemail.com>
2014-04-13 22:47:05 +0100

    Implemented Position.GetDistanceTo

---
+++ GeodesicLibrary/Position.cs
@@ -0,0 +1,103 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Position.cs" company="marknotgeorge">
+//     ©2014 Mark Johnson//
+// </copyright>
+// Based on Javascript code written by and ©2002-2-12 Chris Veness
+// http://www.movable-type.co.uk/scripts/latlong.html
+//-----------------------------------------------------------------------
+using System;
+using UnitsNet;
+using UnitsNet.Units;
+
+namespace Marknotgeorge.GeodesicLibrary
+{
+    public class Position
+    {
+        private Angle _latitude;
+
+        /// <summary>
+        /// Latitude in degrees
+        /// </summary>
+        public double Latitude
+        {
+            get
+            {
+                return _latitude.Degrees;
+            }
+            set
+            {
+                _latitude = Angle.FromDegrees(value);
+            }
+        }
+
+        private Angle _longitude;
+
+        /// <summary>
+        /// Longitude in degrees
+        /// </summary>
+        public double Longitude
+        {
+            get
+            {
+                return _longitude.Degrees;
+            }
+            set
+            {
+                _longitude = Angle.FromDegrees(value);
+            }
+        }
+
+        private Length _radius = Length.FromKilometers(6371);
+
+        /// <summary>
+        /// Initialise a Position, using the default radius of 6371km.
+        /// </summary>
+        /// <param name="lat">Latitude in degrees</param>
+        /// <param name="lon">Longitude in degrees</param>
+        public Position(double lat, double lon)
+        {
+            Latitude = lat;
+            Longitude = lon;
+        }
+
+        /// <summary>
+        /// Initialise a Position with a specific radius.
+        /// </summary>
+        /// <param name="lat">Latitude in degrees.</param>
+        /// <param name="lon">Longitude in degrees.</param>
+        /// <param name="radius">Radius of Earth in kilometres.</param>
+        public Position(double lat, double lon, double radius)
+        {
+            Latitude = lat;
+            Longitude = lon;
+            _radius = Length.FromKilometers(radius);
+        }
+
+        /// <summary>
+        /// Returns the distance from this Position to the supplied Position, using the Haversine
+        /// formula.
+        /// </summary>
+        /// <param name="toPosition">The destination point.</param>
+        /// <param name="unit">The unit of the distance. Defaults to kilometres</param>
+        /// <returns></returns>
+        public double DistanceTo(Position toPosition, LengthUnit unit = LengthUnit.Kilometer)
+        {
+            var lat1 = this._latitude.Radians; var lat2 = toPosition._latitude.Radians;
+            var lon1 = this._longitude.Radians; var lon2 = toPosition._longitude.Radians;
+            var R = _radius.As(unit);
+
+            var dLat = lat2 - lat1;
+            var dLon = lon2 - lon1;
+
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + 
+                Math.Cos(lat1) * Math.Cos(lat2) * 
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c;
+
+            return d;
+        }     
+
+
+    }
+}
---
+++ GeodesicLibraryTests/PositionTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Marknotgeorge.GeodesicLibrary;
+using UnitsNet.Units;
+using UnitsNet;
+
+
+namespace GeodesicLibraryTests
+{
+    /// <summary>
+    /// Summary description for PositionTests
+    /// </summary>
+    [TestClass]
+    public class PositionTests
+    {
+        public PositionTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void GetDistanceToTest()
+        {
+            double expectedValue = 968.9;
+            
+            Position fromPosition = new Position(50.066389, -5.714722);
+            Position toPosition = new Position(58.643889, -3.07);
+
+            double testValue = fromPosition.DistanceTo(toPosition);
+
+            double roundedDistance = Math.Round(testValue, 1);
+
+            Assert.AreEqual(expectedValue, roundedDistance);
+
+        }
+    }
+}
